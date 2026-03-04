6fbaeb1d20d220fbbc5cd733592dc6034413d333
Villkump <villeeemelikumpula@gmail.com>
2026-03-04 12:52:19 +0200

    Oulun alueen koordinaattien haku

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
-
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+
+from urllib.parse import quote
+
+import requests
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')asdasdasdasdasd
+def get_bbox(city):
+    osm_url = f"https://nominatim.openstreetmap.org/search?q={quote(city)}&format=json"
+    headers = {'User-Agent': 'OpenAQCityBBox'}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    response = requests.get(osm_url, headers=headers).json()
+
+    if not response:
+        return None
+
+    # boundingbox sisältää löydetyn kaupungin rajat
+    # siinä on 4 koordinaattipisettä
+    osm_bbox = response[0]['boundingbox']
+
+    # OpenStreetMapin bounding boxin koordinaatit ovat ao järjestyksessä
+    # min_y, max_y, min_x, max_x
+    min_lat, max_lat, min_lon, max_lon = osm_bbox
+
+    # järjestetään uudelleen openAQ:lle sopivaan muotoon: min_x, min_y, max_x, max_y
+    openaq_bbox = f"{min_lon},{min_lat},{max_lon},{max_lat}"
+
+    return openaq_bbox
+
+
+
+
+if __name__ == "__main__":
+    bbox = get_bbox("Oulu")
+    print(bbox)
